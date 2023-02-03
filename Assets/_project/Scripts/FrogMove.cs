32ed5318de01d43df1bdf059c0771cd71fbc280c
ssoprych <sebastian.soprych@gmail.com>
2023-02-03 11:18:12 +0100

    Frog movement, layout

--- Assets/_project/Scripts/FrogMove.cs
+++ Assets/_project/Scripts/FrogMove.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class FrogMove : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _frog;
+    public float PixelsHorizontal;
+    public float PixelsVertical;
   
     void Move()
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            _frog.velocity += new Vector2(-10, 0);
+            _frog.position += Vector2.left * PixelsHorizontal;
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            _frog.position += Vector2.right * PixelsHorizontal;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _frog.position += Vector2.up * PixelsVertical;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _frog.position += Vector2.down * PixelsVertical;
+        }
+        return;
     }
     private void Update()
     {
