7a6b1ceaf08df4edce3b25ae47a735ca7a7374c8
ssoprych <sebastian.soprych@gmail.com>
2023-02-06 23:37:44 +0100

    Level design completed, car scripts and planks

---
+++ Assets/_project/Scripts/Car.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public bool isRight;
+    void Update()
+    {
+        if (isRight)
+        {
+            transform.position += Vector3.right * (Random.Range(3, 10) * Time.deltaTime);
+        }
+        else
+        {
+            transform.position += Vector3.left * (Random.Range(3, 10) * Time.deltaTime);
+        }
+    }
+}
