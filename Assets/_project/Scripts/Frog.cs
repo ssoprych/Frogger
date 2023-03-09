e9a9f7b314ab9e5e6776b354e2464335b9b7723c
ssoprych <sebastian.soprych@gmail.com>
2023-03-09 23:50:20 +0100

    Comments

--- Assets/_project/Scripts/Frog.cs
+++ Assets/_project/Scripts/Frog.cs
@@ -8,7 +8,6 @@ public class Frog : MonoBehaviour
     private Animator _anim;
     public float PixelsHorizontal;
     public float PixelsVertical;
-    public GameObject Plank;
     public GameObject DeathEffect;
     public Transform FrogStartingPoint;
     public bool HealthReduced = false;
@@ -16,48 +15,50 @@ public class Frog : MonoBehaviour
     private void Awake()
     {
         _anim = GetComponent<Animator>();
-        
+        _frog = GetComponent<Rigidbody2D>();
     }
+
+   
 
     void Move()
     {
+        //set all animations bools to false
         _anim.SetBool("IsDown", false);
         _anim.SetBool("IsUp", false);
         _anim.SetBool("IsLeft", false);
         _anim.SetBool("IsRight", false);
+        //set parent to null, move direction and play move animation
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
+            _frog.transform.SetParent(null);
             _frog.position += Vector2.up * PixelsVertical;
             _anim.SetBool("IsUp", true);
+
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
+            _frog.transform.SetParent(null);
             _frog.position += Vector2.left * PixelsHorizontal;
             _anim.SetBool("IsLeft", true);
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            _frog.transform.SetParent(null);
             _frog.position += Vector2.right * PixelsHorizontal;
             _anim.SetBool("IsRight", true);
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
+            _frog.transform.SetParent(null);
             _frog.position += Vector2.down * PixelsVertical;
             _anim.SetBool("IsDown", true);
-        }
-        if (GameManager.Instance.onPlank)
-        {
-            _frog.transform.SetParent(Plank.transform);
-        }
-        else
-        {
-            transform.SetParent(null);
         }
         
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         HealthReduced = false;
         if (collision.gameObject.CompareTag("Plank"))
         {
@@ -86,9 +87,12 @@ public class Frog : MonoBehaviour
             GameManager.Instance.onWater = false;
         }
     }
+
     private void Update()
     {
         Move();
+        
+        //dying to water 
         if (GameManager.Instance.onPlank == false && GameManager.Instance.onWater == true)
         {
             if (HealthReduced == false)
@@ -100,6 +104,7 @@ public class Frog : MonoBehaviour
             }
         }
 
+        //screen boundaries
         if (GameManager.Instance.OutOfScreen)
         {
             Instantiate(DeathEffect, transform.position, transform.rotation);
@@ -108,6 +113,7 @@ public class Frog : MonoBehaviour
             GameManager.Instance.OutOfScreen = false;
         }
 
+        //scoring point by going into one of the house
         if (GameManager.Instance.touchedHouse)
         {
             transform.position = new Vector2(FrogStartingPoint.position.x, FrogStartingPoint.position.y);
@@ -115,6 +121,7 @@ public class Frog : MonoBehaviour
             GameManager.Instance.touchedHouse = false;
         }
 
+        //Ending game
         if (GameManager.Instance.Health == 0 || GameManager.Instance.Score == 3)
         {
             Destroy(gameObject);
