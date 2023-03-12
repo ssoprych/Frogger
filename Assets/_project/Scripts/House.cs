f5bf187ea93dc16bd06cf77a3981a518ecc90971
ssoprych <sebastian.soprych@gmail.com>
2023-03-12 13:02:38 +0100

    Closed and open house sprites

--- Assets/_project/Scripts/House.cs
+++ Assets/_project/Scripts/House.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class House : MonoBehaviour
 {
     public bool Visited;
+    public Sprite[] Houses;
+    public SpriteRenderer spriterenderer;
+    private static int i = 1;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Frog"))
@@ -13,6 +16,7 @@ public class House : MonoBehaviour
             {
                 GameManager.Instance.touchedHouse = true;
                 Visited = true;
+                spriterenderer.sprite = Houses[i];
             }
         }
     }
